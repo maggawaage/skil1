62a14b75629ab52f2c5cc921dd9125c9ff60991e
Andri Sveinbjörnsson <Andri Sveinbjrnsson>
2016-11-29 10:26:19 +0000

    UI v1

--- skilaverkefni/main.cpp
+++ skilaverkefni/main.cpp
@@ -1,8 +1,42 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
 
-int main(int argc, char *argv[])
+using namespace std ;
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+int choice;
+char ch;
 
-    return a.exec();
+do{
+cout<<endl;
+cout<<"Please choose from the following: \n";
+cout<<"\t1. Write Data.  \n";
+cout<<"\t2. Read Data. \n";
+cout<<"\t3. Quit. \n";
+cout<<endl;
+cout<<"Your choice: ";
+cin>>choice;
+
+switch(choice)
+{
+    case 1:
+        write();
+        break;
+    case 2:
+        read();
+        break;
+    case 3:
+
+        break;
+    default:
+        cout<<"\tInvalid entry!"<<endl;
+}
+cout<<"\nTry again (y/n): ";
+cin>>ch;
+
+}while(ch == 'y' || ch == 'Y');
+
+return 0;
 }
