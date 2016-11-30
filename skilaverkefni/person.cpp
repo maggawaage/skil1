8d29fe1a256d2f4425a597c989e457db0a876d84
fridrik stefansson <fridrikstefansson@fridriks-MacBook-Air.local>
2016-11-30 13:16:19 +0000

    k

--- skilaverkefni/person.cpp
+++ skilaverkefni/person.cpp
@@ -59,3 +59,67 @@ int Person::compareDeathReversed(Person a, Person b)
 {
     return a._death > b._death;
 }
+
+vector<Person> Person::SearchName(vector <Person> list, string searchName)
+{
+    vector <Person> newList;
+
+    for (int i = 0; i < list.size(); i++)
+    {
+
+        if (list[i].name.find(searchName) != std::string::npos)
+        {
+            newList.push_back(list[i]);
+        }
+    }
+
+    return newList;
+}
+
+vector<Person> Person::SearchGender(vector <Person> list, char searchGender)
+{
+    vector <Person> newList;
+
+    for (int i = 0; i < list.size(); i++)
+    {
+
+        if (list[i].gender == searchGender)
+        {
+            newList.push_back(list[i]);
+        }
+    }
+
+    return newList;
+}
+
+vector<Person> Person::SearchBirth(vector <Person> list, int searchBirth)
+{
+    vector <Person> newList;
+
+    for (int i = 0; i < list.size(); i++)
+    {
+
+        if (list[i].birth == searchBirth)
+        {
+            newList.push_back(list[i]);
+        }
+    }
+
+    return newList;
+}
+
+vector<Person> Person::SearchDeath(vector <Person> list, int searchDeath)
+{
+    vector <Person> newList;
+
+    for (int i = 0; i < list.size(); i++)
+    {
+
+        if (list[i].death == searchDeath)
+        {
+            newList.push_back(list[i]);
+        }
+    }
+
+    return newList;
+}
