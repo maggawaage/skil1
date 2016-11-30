97ff59f1095153a7710be5b291d7e9867afc6e05
soleyas <soleyasgeirsdottir@Soleys-MacBook-Air.local>
2016-11-30 12:20:03 +0000

    heyehey

--- skilaverkefni/personservice.h
+++ skilaverkefni/personservice.h
@@ -12,6 +12,16 @@ public:
 
     //vector<Person> getPerformers(/*Todo parameters*/);
     vector<Person> alpha();
+<<<<<<< HEAD
+=======
+    vector<Person> realpha()
+    vector<Person> year();
+    vector<Person> reyear()
+    vector<Person> gender();
+    vector<Person> regender();
+    vector<Person> death();
+    vector<Person> redeath();
+>>>>>>> c59ca2856921197fcd902f66a3322b4a37f1a30c
 };
 
 #endif // PERSONRSERVICE_H
